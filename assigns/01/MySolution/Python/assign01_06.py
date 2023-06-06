d6e7e53c88630fcfd5f90bca5afd82901f00bfd0
Jenny Jung <jennyjung@crc-dot1x-nat-10-239-230-206.bu.edu>
2023-06-05 22:18:43 -0400

    final save

--- assigns/01/MySolution/Python/assign01_06.py
+++ assigns/01/MySolution/Python/assign01_06.py
@@ -15,30 +15,31 @@ def mylist_append(xs, ys):
     if mylist_nilq(xs):
         return ys
     else:
-        x1 = xs.cons1
-        xs_rest = xs.cons2
-        return mylist_cons(x1, mylist_append(xs_rest, ys))
+        x1 = xs.get_cons1()
+        x2 = xs.get_cons2()
+        return mylist_cons(x1, mylist_append(x2, ys))
 
 def mylist_quicksort(xs):
-    def myqsort(xs):
+    def qsort(xs):
         if mylist_nilq(xs): 
             return mylist_nil()
         else:
-            x1 = xs.cons1
-            xs_rest = xs.cons2
-            ys, zs = qpart(xs_rest, x1)
-            ys = myqsort(ys) 
-            zs = myqsort(zs)
+            x1 = xs.get_cons1()
+            x2 = xs.get_cons2()
+            ys, zs = qpart(x2, x1)
+            ys = qsort(ys) 
+            zs = qsort(zs)
             return mylist_append(mylist_append(ys, mylist_cons(x1,mylist_nil())),zs)
+        
     def qpart(xs, p0):
         if mylist_nilq(xs): 
             return (mylist_nil(), mylist_nil())
         else:
-            x1 = xs.cons1
-            xs_rest = xs.cons2
-            ys, zs = qpart(xs_rest, p0)
+            x1 = xs.get_cons1()
+            x2 = xs.get_cons2()
+            ys, zs = qpart(x2, p0)
             if x1 <= p0: 
                 return (mylist_cons(x1, ys), zs)
             else:
                 return (ys, mylist_cons(x1, zs))
-    return (myqsort(xs))
+    return (qsort(xs))   
--- assigns/01/MySolution/Python/assign01_07.py
+++ assigns/01/MySolution/Python/assign01_07.py
@@ -15,43 +15,43 @@ def mylist_mergesort(xs):
         if mylist_nilq(xs):
             return (mylist_nil(), mylist_nil())
         else:
-            x1 = xs.cons1
-            xs_rest = xs.cons2
-            if mylist_nilq(xs_rest):
+            x1 = xs.get_cons1()
+            x2 = xs.get_cons2()
+            if mylist_nilq(x2):
                 return (mylist_cons(x1, mylist_nil()),mylist_nil())
             else:
-                x2 = xs_rest.cons1
-                xs_rest = xs_rest.cons2
-                (ys, zs) = split(xs_rest)
-                return (mylist_cons(x1,ys), mylist_cons(x2,zs))
+                y1 = x2.get_cons1()
+                y2 = x2.get_cons2()
+                (ys, zs) = split(y2)
+                return (mylist_cons(x1,ys), mylist_cons(y1,zs))
 
 
     def merge(ys, zs):
         if mylist_nilq(ys):
             return zs
         else: 
-            y1 = ys.cons1
-            ys_rest = ys.cons2
+            y1 = ys.get_cons1()
+            y2 = ys.get_cons2()
             if mylist_nilq(zs):
-                return (mylist_cons(y1, ys_rest))
+                return (mylist_cons(y1, y2))
             else:
-                z1 = zs.cons1
-                zs_rest = zs.cons2
+                z1 = zs.get_cons1()
+                z2 = zs.get_cons2()
                 if (y1 <= z1):
-                    return (mylist_cons(y1, merge(ys_rest, mylist_cons(z1,zs_rest))))
+                    return (mylist_cons(y1, merge(y2, mylist_cons(z1,z2))))
                 else:
-                    return (mylist_cons(z1, merge(mylist_cons(y1, ys_rest),zs_rest)))
+                    return (mylist_cons(z1, merge(mylist_cons(y1, y2),z2)))
     
     if mylist_nilq(xs):
         return mylist_nil()
     else:
-        x1 = xs.cons1
-        xs_rest = xs.cons2
-        if mylist_nilq(xs_rest):
+        x1 = xs.get_cons1()
+        x2 = xs.get_cons2()
+        if mylist_nilq(x2):
             return mylist_cons(x1, mylist_nil())
         else:
-            x2 = xs_rest.cons1
-            xs_rest = xs_rest.cons2
-            ys, zs = split(xs_rest)
-            return merge(mylist_mergesort(mylist_cons(x1, ys)),mylist_mergesort(mylist_cons(x2, zs)))
+            y1 = x2.get_cons1()
+            y2 = x2.get_cons2()
+            ys, zs = split(y2)
+            return merge(mylist_mergesort(mylist_cons(x1, ys)),mylist_mergesort(mylist_cons(y1, zs)))
     
